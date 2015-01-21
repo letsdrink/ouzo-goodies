e1590fd5ce3569f9e57d1233eab43832a3c11563
piotrooo <piotroo89@gmail.com>
2015-01-21 15:13:18 +0100

    Added Booleans class util.

---
+++ Utilities/Booleans.php
@@ -0,0 +1,30 @@
+<?php
+/*
+ * Copyright (c) Ouzo contributors, http://ouzoframework.org
+ * This file is made available under the MIT License (view the LICENSE file for more information).
+ */
+namespace Ouzo\Utilities;
+
+/**
+ * Class Booleans
+ * @package Ouzo\Utilities
+ */
+class Booleans
+{
+    /**
+     * @param $string
+     * @return bool
+     */
+    public static function toBoolean($string)
+    {
+        $string = strtolower($string);
+        $specials = array('true', 'on', 'yes');
+        if (in_array($string, $specials)) {
+            return true;
+        }
+        if (Strings::equal($string, 'false')) {
+            return false;
+        }
+        return !is_string($string);
+    }
+}
