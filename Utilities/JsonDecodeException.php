ea59c14e4105db90bc183f17c066b23143f6b35a
Daniel <wilkowski.kontakt@gmail.com>
2016-10-07 18:09:23 +0200

    Displaying slightly more verbose message in JsonDecodeException

--- Utilities/JsonDecodeException.php
+++ Utilities/JsonDecodeException.php
@@ -4,4 +4,8 @@ namespace Ouzo\Utilities;
 
 class JsonDecodeException extends \Exception
 {
+    public function __construct($lastErrorMessage = "", $lastErrorCode = 0)
+    {
+        parent::__construct('JSON decode error: ' . $lastErrorMessage, $lastErrorCode);
+    }
 }
