2e0dae3de69cdd9ef53c0eebf3163ff9d8e5baea
Daniel <wilkowski.kontakt@gmail.com>
2016-11-23 15:31:48 +0100

    Test for Strings::replaceNth()

--- Utilities/Strings.php
+++ Utilities/Strings.php
@@ -604,7 +604,10 @@ class Strings
     public static function substringAfter($string, $separator)
     {
         $pos = mb_strpos($string, $separator);
-        return $pos !== false ? mb_substr($string, $pos + 1, mb_strlen($string)) : $string;
+        if ($pos === false) {
+            return $string;
+        }
+        return mb_substr($string, $pos + 1, mb_strlen($string));
     }
 
     /**
