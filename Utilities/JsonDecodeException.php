44c5100c6a6aa8b9211ad08045669b2a7fed2ac8
Daniel <wilkowski.kontakt@gmail.com>
2016-12-02 12:53:47 +0100

    Added ouzo license header to JsonDecodeException

--- Utilities/JsonDecodeException.php
+++ Utilities/JsonDecodeException.php
@@ -1,4 +1,8 @@
 <?php
+/*
+ * Copyright (c) Ouzo contributors, http://ouzoframework.org
+ * This file is made available under the MIT License (view the LICENSE file for more information).
+ */
 namespace Ouzo\Utilities;
 
 
