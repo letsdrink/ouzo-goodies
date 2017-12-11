cccddb5311afbb20821842dd665a48ca26a3a4e8
dpawlik <dominik.pawlik@thulium.pl>
2017-12-11 11:49:01 +0100

    Few words of docs in CatchException class

--- Tests/CatchException.php
+++ Tests/CatchException.php
@@ -8,6 +8,22 @@ namespace Ouzo\Tests;
 
 use Throwable;
 
+/**
+ * Class CatchException can be used as alternative to try{...}catch(...){...} block in tests.
+ * It can be used in presented way:
+ *
+ * class ThrowableClass{
+ *      public function throwableMethod(){...}
+ * }
+ *
+ * $throwableObject = new ThrowableClass();
+ *
+ * CatchException::when($throwableObject)->throwableMethod();
+ *
+ * CatchException::assertThat()->isInstanceOf(ThrowableClass::class);
+ *
+ * @package Ouzo\Tests
+ */
 class CatchException
 {
     /** @var Throwable|null */
