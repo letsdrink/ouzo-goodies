ec42a18ac222aefbd7346dedd3f657b5322d2227
aoczkowicz <andrzej.oczkowicz@thulium.pl>
2018-09-28 10:57:31 +0200

    Add "Preserve keys" option to Batching iterator

--- Utilities/Iterator/BatchingIterator.php
+++ Utilities/Iterator/BatchingIterator.php
@@ -3,12 +3,15 @@
  * Copyright (c) Ouzo contributors, http://ouzoframework.org
  * This file is made available under the MIT License (view the LICENSE file for more information).
  */
+
 namespace Ouzo\Utilities\Iterator;
 
 use Iterator;
 
 class BatchingIterator implements Iterator
 {
+    const OPTION_PRESERVER_KEYS = 0x2;
+
     /** @var Iterator $iterator */
     private $iterator;
     /** @var int */
@@ -17,15 +20,18 @@ class BatchingIterator implements Iterator
     private $currentChunk;
     /** @var int */
     private $position = 0;
+    /** @var bool */
+    private $preserveKeys;
 
     /**
      * @param Iterator $iterator
      * @param int $chunkSize
      */
-    public function __construct(Iterator $iterator, $chunkSize)
+    public function __construct(Iterator $iterator, $chunkSize, $options = 0)
     {
         $this->iterator = $iterator;
         $this->chunkSize = $chunkSize;
+        $this->preserveKeys = $options && self::OPTION_PRESERVER_KEYS;
     }
 
     /**
@@ -80,7 +86,8 @@ class BatchingIterator implements Iterator
     {
         $this->currentChunk = [];
         for ($i = 0; $i < $this->chunkSize && $this->iterator->valid(); $i++, $this->iterator->next()) {
-            $this->currentChunk[] = $this->iterator->current();
+            $key = $this->preserveKeys ? $this->iterator->key() : $i;
+            $this->currentChunk[$key] = $this->iterator->current();
         }
     }
 }
