3b95ebeb1aeffac24114b5019d79bbbdf59ece59
Bartosz Bańkowski <bartosz.bankowski@thulium.pl>
2021-03-25 17:05:18 +0100

    [php8] Refactoring code to use PHP 8 features - fixed copyright notice.

--- Utilities/Booleans.php
+++ Utilities/Booleans.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * Copyright (c) Ouzo contributors, http://ouzoframework.org
+ * Copyright (c) Ouzo contributors, https://github.com/letsdrink/ouzo
  * This file is made available under the MIT License (view the LICENSE file for more information).
  */
 namespace Ouzo\Utilities;
--- Utilities/Chain/EndChain.php
+++ Utilities/Chain/EndChain.php
@@ -1,4 +1,8 @@
 <?php
+/*
+ * Copyright (c) Ouzo contributors, https://github.com/letsdrink/ouzo
+ * This file is made available under the MIT License (view the LICENSE file for more information).
+ */
 
 
 namespace Ouzo\Utilities\Chain;
