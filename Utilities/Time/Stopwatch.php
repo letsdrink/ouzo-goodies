7846f8e9711323a51626893925dde2043ed1e9e4
Piotr Olaszewski <piotr.olaszewski@thulium.pl>
2022-05-16 09:15:14 +0200

    Cleanup Stopwatch api

--- Utilities/Time/Stopwatch.php
+++ Utilities/Time/Stopwatch.php
@@ -10,22 +10,23 @@ use LogicException;
 
 class Stopwatch
 {
+    private Ticker $ticker;
+
     private bool $running = false;
     private int $startedAt;
     private int $elapsedNanos = 0;
-    private Ticker $ticker;
 
     private function __construct(?Ticker $ticker)
     {
         $this->ticker = $ticker ?: new Ticker();
     }
 
-    public static function createUnstarted(?Ticker $ticker): self
+    public static function createUnstarted(?Ticker $ticker = null): self
     {
         return new Stopwatch($ticker);
     }
 
-    public static function createStarted(?Ticker $ticker): self
+    public static function createStarted(?Ticker $ticker = null): self
     {
         return self::createUnstarted($ticker)->start();
     }
